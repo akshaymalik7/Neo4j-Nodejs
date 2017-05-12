5efd57c1479f63d65094d61d4c34406085b8e703
Akshay Malik <ak.shay.penn@gmail.com>
2017-05-11 20:21:27 -0400

    Update mapLogic.js

--- public/js/mapLogic.js
+++ public/js/mapLogic.js
@@ -6,77 +6,135 @@ var map = new mapboxgl.Map({
     center: [77.4126, 23.2599],
     zoom: 4,
     bearing: 15,
-    pitch: 60,
+    pitch: 75,
     interactive: false
 });
 
 
-
-
+// pixels the map pans when the up or down arrow is clicked
 var deltaDistance = 100;
 
-    // degrees the map rotates when the left or right arrow is clicked
-    var deltaDegrees = 25;
+// degrees the map rotates when the left or right arrow is clicked
+var deltaDegrees = 25;
 
-    function easing(t) {
-        return t * (2 - t);
-    }
-
-
-function parseWKTPolygon(wkt) {
-    var pointArr = [];
-    var points = wkt.slice(10, -3).split(",");
-
-    points.forEach( function(i,v) {
-        var point = $.trim(v).split(" ");
-        var xy = [Number(point[1]), Number(point[0])];
-        pointArr.push(xy)
-    });
-
-    return pointArr;
+function easing(t) {
+    return t * (2 - t);
 }
-
 
 
 
 map.on('load', function () {
 
-    map.addLayer({
-        'id': 'districts',
-        'type': 'fill',
-        'source': {
-            'type': 'geojson',
-            'data': "https://gist.githubusercontent.com/anonymous/21ecd012ca806bd52cfdc37772ee2b85/raw/c0694d9f07998faf9d4038ffb2203e574e1a5c02/map.geojson"
-        },
-        'layout': {},
-        'paint': {
-            'fill-color': '#088',
-            'fill-opacity': 0.8
-        }
-    });
-    
-    map.getCanvas().focus();
 
-        map.getCanvas().addEventListener('keydown', function(e) {
-            e.preventDefault();
-            if (e.which === 38) { // up
-                map.panBy([0, -deltaDistance], {
-                    easing: easing
-                });
-            } else if (e.which === 40) { // down
-                map.panBy([0, deltaDistance], {
-                    easing: easing
-                });
-            } else if (e.which === 37) { // left
-                map.easeTo({
-                    bearing: map.getBearing() - deltaDegrees,
-                    easing: easing
-                });
-            } else if (e.which === 39) { // right
-                map.easeTo({
-                    bearing: map.getBearing() + deltaDegrees,
-                    easing: easing
-                });
-            }
-        }, true);
+  map.getCanvas().focus();
+
+      map.getCanvas().addEventListener('keydown', function(e) {
+          e.preventDefault();
+          if (e.which === 38) { // up
+              map.panBy([0, -deltaDistance], {
+                  easing: easing
+              });
+          } else if (e.which === 40) { // down
+              map.panBy([0, deltaDistance], {
+                  easing: easing
+              });
+          } else if (e.which === 37) { // left
+              map.easeTo({
+                  bearing: map.getBearing() - deltaDegrees,
+                  easing: easing
+              });
+          } else if (e.which === 39) { // right
+              map.easeTo({
+                  bearing: map.getBearing() + deltaDegrees,
+                  easing: easing
+              });
+          }
+      }, true);
+
+    map.addSource("allDistricts", {
+            "type": "geojson",
+            "data": "https://gist.githubusercontent.com/anonymous/21ecd012ca806bd52cfdc37772ee2b85/raw/c0694d9f07998faf9d4038ffb2203e574e1a5c02/map.geojson"
+        });
+
+
+
+        map.addLayer({
+              "id": "fills",
+              "type": "fill",
+              "source": "allDistricts",
+              "layout": {},
+              "paint": {
+                  "fill-color": "#FFBA49",
+                  "fill-opacity": 0.4
+              }
+          });
+
+          map.addLayer({
+              "id": "borders",
+              "type": "line",
+              "source": "allDistricts",
+              "layout": {},
+              "paint": {
+                  "line-color": "#EF5B5B",
+                  "line-width": 1,
+                  "line-opacity": 0.3
+              }
+          });
+
+    console.log(collectResponse);
 });
+
+
+var Population = function() {
+
+  collectResponse.forEach(function(district, index){
+
+        map.addLayer({
+            'id': index + "district",
+            'type': 'fill-extrusion',
+            'source': {
+                'type': 'geojson',
+                'data':{
+                          "type": "FeatureCollection",
+                          "features": [
+                            {
+                              "type": "Feature",
+                              "properties": {
+                                "height": district.height,
+                                "color": "red"
+                              },
+                              "geometry": {
+                                "type": "MultiPolygon",
+                                "coordinates": Terraformer.WKT.parse(district.wkt).coordinates
+
+
+                              }
+                            }
+                          ]
+                        }
+                      },
+                      'paint': {
+                          // See the Mapbox Style Spec for details on property functions
+                          // https://www.mapbox.com/mapbox-gl-style-spec/#types-function
+                          'fill-extrusion-color': {
+                              // Get the fill-extrusion-color from the source 'color' property.
+                              'property': 'color',
+                              'type': 'identity'
+                          },
+                          'fill-extrusion-height': {
+                              // Get fill-extrusion-height from the source 'height' property.
+                              'property': 'base_height',
+                              'type': 'identity'
+                          },
+                          'fill-extrusion-base': {
+                              // Get fill-extrusion-base from the source 'base_height' property.
+                              'property': 'height',
+                              'type': 'identity'
+                          },
+                          // Make extrusions slightly opaque for see through indoor walls.
+                          'fill-extrusion-opacity': 1
+                      }
+
+          });
+      });
+    }
